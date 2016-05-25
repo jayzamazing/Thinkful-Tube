2b3d3afb0f8f9843783372c19ea6d81156ef25a8
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-25 10:19:47 -0400

    Added index.html and app.js, added bootstrap normalize and ekko-lightbox css to index.html, added jquery bootstrap ekko and app.js to index.html. Added form with input for searching in idnex.html. Added search-term submit, getrequest, showresults, delegate functions to app.js.

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(function() {
+  $('#search-term').submit(function(e) {
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+    e.preventDefault();
+  });
+});
+function getRequest(searchTerm) {
+  var params = {
+    key: 'AIzaSyC-rNio-jPP6kb1x-hq-eKB24ofZ5Pmjtw',
+    q: searchTerm,
+    part: 'snippet'
+  };
+  url = 'https://www.googleapis.com/youtube/v3/search';
+  $.getJSON(url, params, function(data) {
+    showResults(data.items);
+  });
+}
+function showResults(results) {
+  var html = "";
+  $.each(results, function(index, value){
+    html += '<a href="https://www.youtube.com/watch?v=' + value.id.videoId + '" data-toggle="lightbox"><img src="' + value.snippet.thumbnails.medium.url + '"></a>';
+    console.log(value.snippet.thumbnails.medium.url);
+  });
+  $('#search-results').html(html);
+}
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
